5af2a1365ff05d3ed2afdf4f49fab673a06e79d5
yaozm <ityaozm@gmail.com>
2021-05-21 18:14:16 +0800

    Update IDE config file

--- examples/_ide_helper.php
+++ examples/_ide_helper.php
@@ -1,27 +1,30 @@
 <?php
 
-/**
- * @property Application $app
- * @property \Guanguans\EiiFoundation\Di\Container $container
- * @property array $aliases
- *
- * @method static getAlias($alias, $throwException = true)
- * @method static getRootAlias($alias)
- * @method static setAlias($alias, $path)
- * @method static createObject($type, array $params = [])
- * @method static createContainer($type, array $params = [])
- * @method static configure($object, $properties)
- */
-class Eii
-{
-}
-
-/**
- * @property \Guanguans\EiiFoundation\Examples\Components\ExampleComponent $example
- */
-class Application
-{
-}
+// /**
+//  * @property MyApplication $app
+//  * @property \Guanguans\EiiFoundation\Di\Container $container
+//  * @property array $aliases
+//  *
+//  * @method static getAlias($alias, $throwException = true)
+//  * @method static getRootAlias($alias)
+//  * @method static setAlias($alias, $path)
+//  * @method static createObject($type, array $params = [])
+//  * @method static createContainer($type, array $params = [])
+//  * @method static configure($object, $properties)
+//  */
+// class Eii
+// {
+//     public static $app;
+//     public static $container;
+//     public static $aliases;
+// }
+//
+// /**
+//  * @property \Guanguans\EiiFoundation\Examples\Components\ExampleComponent $example
+//  */
+// class MyApplication
+// {
+// }
 
 namespace Guanguans\EiiFoundation\Examples\Components {
 
