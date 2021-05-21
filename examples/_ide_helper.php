9124737c7f2588fa0f7bba4a3bbd726bee767a44
yaozm <ityaozm@gmail.com>
2021-05-21 17:56:10 +0800

    Update IDE config file

--- examples/_ide_helper.php
+++ examples/_ide_helper.php
@@ -1,30 +1,34 @@
 <?php
 
 /**
- * Class Eii.
+ * @property Application $app
+ * @property \Guanguans\EiiFoundation\Di\Container $container
+ * @property array $aliases
+ *
+ * @method static getAlias($alias, $throwException = true)
+ * @method static getRootAlias($alias)
+ * @method static setAlias($alias, $path)
+ * @method static createObject($type, array $params = [])
+ * @method static createContainer($type, array $params = [])
+ * @method static configure($object, $properties)
  */
 class Eii
 {
-    /**
-     * @var MyApplication
-     */
-    public static $app;
 }
 
 /**
  * @property \Guanguans\EiiFoundation\Examples\Components\ExampleComponent $example
- * Class MyApplication
  */
-class MyApplication
+class Application
 {
 }
 
 namespace Guanguans\EiiFoundation\Examples\Components {
 
     /**
-     * Class ExampleComponent.
-     * @method behaviorMethod()
      * @property string $behaviorName
+     *
+     * @method behaviorMethod()
      */
     class ExampleComponent
     {
