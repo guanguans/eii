b11c2190aa3387d9043519b4b7c089dba51b94cb
yaozm <ityaozm@gmail.com>
2021-05-21 17:25:36 +0800

    Fix style

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -2,7 +2,7 @@
 
 /**
  * This file is part of the guanguans/eii-foundation.
- * This file modified from https://github.com/yiisoft/yii2.
+ * This file is modified from https://github.com/yiisoft/yii2
  * (c) guanguans <ityaozm@gmail.com>
  * This source file is subject to the MIT license that is bundled.
  */
